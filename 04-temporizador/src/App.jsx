bb2efa54d08e2cb9c5a5bc8073fe38f4be7d32dd
IsraChido <2020030223@upsin.edu.mx>
2023-04-18 14:55:52 -0700

    Cuarta práctica

---
+++ 04-temporizador/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Countdown from './countdown/Countdown';
+
+function App() {
+  return(
+    <div>
+      <Countdown />
+    </div>
+  );
+}
+
+export default App
